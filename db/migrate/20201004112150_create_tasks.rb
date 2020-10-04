a7e52364d210d3b6e4cb5151afa8e7b4c3e87f4b
Mesi21 <mse212002@gmail.com>
2020-10-04 14:30:10 +0300

    :construction: added controller for tasks and projects

---
+++ db/migrate/20201004112150_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :content
+      t.references :project, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
